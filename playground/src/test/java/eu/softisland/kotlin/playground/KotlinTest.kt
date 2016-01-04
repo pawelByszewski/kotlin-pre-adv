af8fbd4bc2edc1cec8df75e7146bcd9b4d0ff903
Pawel Byszewski <pawell.b@gmail.com>
2016-01-04 12:56:06 +0100

    activity and sample test in Kotlin

---
+++ playground/src/test/java/eu/softisland/kotlin/playground/KotlinTest.kt
@@ -0,0 +1,15 @@
+package eu.softisland.kotlin.playground
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+
+class KotlinTest {
+
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, (2 + 2).toLong())
+    }
+}
+
+
