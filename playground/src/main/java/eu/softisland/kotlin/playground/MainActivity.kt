2c5c7ae9deef87d6e7d56d4d8abaa46208f928c2
Pawel Byszewski <pawell.b@gmail.com>
2016-01-04 13:59:05 +0100

    bind services; benchmark test

--- playground/src/main/java/eu/softisland/kotlin/playground/MainActivity.kt
+++ playground/src/main/java/eu/softisland/kotlin/playground/MainActivity.kt
@@ -1,12 +1,55 @@
 package eu.softisland.kotlin.playground
 
+import android.app.Activity
+import android.app.usage.NetworkStatsManager
+import android.content.Context
+import android.media.AudioManager
+import android.net.ConnectivityManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Vibrator
+import android.os.storage.StorageManager
+import android.support.v4.app.Fragment
+import android.widget.TextView
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
 
 class MainActivity : AppCompatActivity() {
+
+    val audioManager: AudioManager by bindService(Context.AUDIO_SERVICE)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        (findViewById(R.id.ringerMode) as TextView).text = "${audioManager.ringerMode}"
+    }
+}
+
+
+public fun <V> Activity.bindService(serviceName: String)
+        : ReadOnlyProperty<Activity, V> = fetchService(serviceName, Activity::getSystemService)
+
+private fun Fragment.serviceBinder(serviceName: String): Any {
+    return activity.getSystemService(serviceName)
+}
+
+@Suppress("UNCHECKED_CAST")
+private fun <T, V> fetchService(serviceName: String, binder: T.(String) -> Any)
+        = Lazy { t: T, desc -> t.binder(serviceName) as V? ?: viewNotFound(serviceName, desc) }
+
+private fun viewNotFound(serviceName: String, desc: KProperty<*>): Nothing =
+        throw IllegalStateException("System service $serviceName for '${desc.name}' not found.")
+
+// Like Kotlin's lazy delegate but the initializer gets the target and metadata passed to it
+private class Lazy<T, V>(private val initializer: (T, KProperty<*>) -> V) : ReadOnlyProperty<T, V> {
+    private object EMPTY
+    private var value: Any? = EMPTY
+
+    override fun getValue(thisRef: T, property: KProperty<*>): V {
+        if (value == EMPTY) {
+            value = initializer(thisRef, property)
+        }
+        @Suppress("UNCHECKED_CAST")
+        return value as V
     }
 }
--- playground/src/test/java/eu/softisland/kotlin/playground/KotlinTest.kt
+++ playground/src/test/java/eu/softisland/kotlin/playground/KotlinTest.kt
@@ -4,12 +4,25 @@ import org.junit.Test
 import org.assertj.core.api.Assertions.assertThat
 
 
-class KotlinTest {
+class BenchmarkTest {
+    val iterationsCount = 200
+    var totalTime = 0L
+
+    fun test(a: Long, b: String, c: LongRange) {
+        ++totalTime
+        test(a,b,c)
+    }
+
+    private fun calculate() = (1L..5000000L).asSequence().map { it * 40 }.reduce { a,b -> (a*b)%19 + 1 }
 
     @Test
-    fun addition_isCorrect() {
-        assertThat(4).isEqualTo(2 + 2)
+    fun almostBenchmark() {
+        //        iterationsCount.downTo(1).forEach { test() }
+        try {
+            test(1234, "iusdhfpaisdhgpaiouwrhgaoergnaoig", (1L..5000000L))
+        } catch( t: Throwable) {
+            println(totalTime)
+        }
+        println("Avg time: " + (totalTime.toDouble() / iterationsCount))
     }
 }
-
-
